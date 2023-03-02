bd42932690e58a0b868240545ecdd4f3d3259713
jeann26 <124705872+jeann26@users.noreply.github.com>
2023-03-02 08:48:25 -0500

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,7 @@
+function show() {
+    document.getElementById('sidebar').classList.toggle('active');}
+
+    $("#btn-id").click(function(){
+        $("#form-id")[0].reset();
+        return false; 
+    })
